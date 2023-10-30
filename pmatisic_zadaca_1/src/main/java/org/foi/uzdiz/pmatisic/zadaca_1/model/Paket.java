f61798fefa69a89cfc362803e99de549e54e216a
Petar M <pmatisic@foi.hr>
2023-10-30 03:41:28 +0100

    modified paket record

--- pmatisic_zadaca_1/src/main/java/org/foi/uzdiz/pmatisic/zadaca_1/model/Paket.java
+++ pmatisic_zadaca_1/src/main/java/org/foi/uzdiz/pmatisic/zadaca_1/model/Paket.java
@@ -6,7 +6,6 @@ import java.time.format.DateTimeFormatter;
 public record Paket(String oznaka, LocalDateTime vrijemePrijema, String posiljatelj,
     String primatelj, String vrstaPaketa, double visina, double sirina, double duzina,
     double tezina, UslugaDostave uslugaDostave, double iznosPouzeca) {
-
   public static LocalDateTime konvertirajVrijeme(String vrijeme) {
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy. HH:mm:ss");
     return LocalDateTime.parse(vrijeme, formatter);
